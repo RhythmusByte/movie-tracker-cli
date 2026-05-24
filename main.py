dcf1cfc1d021f92dfca860fcd76df511ba1b2aa4
Akhil Mahesh <contact@akhilmahesh.com>
2026-05-24 21:28:15 +0530

    feat: main file for testing api connection

---
+++ main.py
@@ -0,0 +1,26 @@
+import requests
+from dotenv import load_dotenv
+import os
+from data_fetcher import Fetcher
+
+load_dotenv()
+API_KEY = os.getenv('API_KEY')
+
+
+url = "https://api.themoviedb.org/3/trending/all/day?language=en-US"
+
+headers = {
+    "accept": "application/json",
+    "Authorization": f"Bearer {API_KEY}"
+}
+
+class Movie(Fetcher):
+    def __init__(self):
+        super().__init__(url, headers)
+        
+    def data(self):
+        self.data = self.fetch()
+        print(self.data.text)
+
+m = Movie()
+m.data()
