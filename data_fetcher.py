f1ef1c26d1cfa70f3fdbc6c662b702e9d5d17380
Akhil Mahesh <contact@akhilmahesh.com>
2026-05-24 20:41:10 +0530

    feat(fetcher): add fetcher class to fetch data from the given base url and api.

---
+++ data_fetcher.py
@@ -0,0 +1,14 @@
+import requests
+from rich import print
+
+class Fetcher:
+    def __init__(self, base_url, headers):
+        self.base_url = base_url
+        self.headers = headers
+
+    def fetch():
+        r = requests.get(self.base_url, headers=self.headers)
+        if r.status_code == 200:
+            return r 
+        else:
+            r.raise_for_status()
