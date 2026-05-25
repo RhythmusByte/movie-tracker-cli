886e42907319d0660b49207660e756b328624d77
Akhil Mahesh <contact@akhilmahesh.com>
2026-05-25 21:18:48 +0530

    refactor(Fetcher): add a class method for three args

--- Fetcher.py
+++ Fetcher.py
@@ -5,6 +5,12 @@ class Fetcher:
     def __init__(self, base_url, headers):
         self.base_url = base_url
         self.headers = headers
+        
+    @classmethod
+    def with_param(cls, base_url, headers, params):
+        instance = cls(base_url, headers)
+        instance.params = params
+        return instance
 
     def fetch(self):
         r = requests.get(self.base_url, headers=self.headers)
