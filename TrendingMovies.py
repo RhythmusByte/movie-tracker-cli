57e90e54b3965e54cfd1e72e258fda903df4d49b
Akhil Mahesh <contact@akhilmahesh.com>
2026-05-25 21:19:23 +0530

    feat(TrendingMovies): add base structure for trending movies class

---
+++ TrendingMovies.py
@@ -0,0 +1,35 @@
+import os
+import requests
+from dotenv import load_dotenv
+from Fetcher import Fetcher
+from rich import print
+
+load_dotenv()
+API_KEY = os.getenv('API_KEY')
+
+params = 'language=en-US'
+
+url = 'https://api.themoviedb.org/3/trending/movie/'
+
+headers = {
+    "accept": "application/json",
+    "Authorization": f"Bearer {API_KEY}"
+}
+
+class TrendingMovies(Fetcher):
+    def time_window(self):
+        while True:
+            user_choice = input('Enter the time window for the list (d for day, w for week): ').lower()
+            
+            if user_choice == 'd':
+                return 'day'
+            elif user_choice == 'w':
+                return 'week'
+            else:
+                print('[red]Wrong Input!![/red]')
+    
+    def __init__(self):
+        tw = self.time_window()
+        structured_url = f'{url}{tw}'
+        super().__init__(structured_url, headers=headers)
+        
